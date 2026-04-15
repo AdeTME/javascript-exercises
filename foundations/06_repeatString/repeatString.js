04dab8f22b03da355913990008195577aec8f207
Adetunji Okekunle <tjokekunle@gmail.com>
2026-04-15 22:29:34 +0100

    solving repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function (str,num){
+        let addstr="";
+        for (let i = 0;i<num;i++){
+            addstr+=str;
+        }
+        if (num<0){
+		return "ERROR"
+        } else{
+            return addstr;
+        }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
