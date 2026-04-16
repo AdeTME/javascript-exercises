43b396757558aa491a5b540898265d053edb4ea2
Adetunji Okekunle <tjokekunle@gmail.com>
2026-04-16 20:56:28 +0100

    I reached removeFromArray, but i need to lock in on some of the loop syntax

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function removefromarray(arr, ...arg) {
+	const result =[];
+    for (let i of arg){
+        arr = arr.filter((val)=>val !== i);
+    }
+    return arr;
 };
 
 // Do not edit below this line
