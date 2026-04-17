22eff58418b6b37783d344a10f4bc380ef402ecf
Adetunji Okekunle <tjokekunle@gmail.com>
2026-04-17 20:47:18 +0100

    did the tempconversion, learned the parseint and unary function

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+   let result = ((temp-32)/1.8);
+   return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    let result =((temp*1.8)+32);
+    return +result.toFixed(1);
 };
 
 // Do not edit below this line
