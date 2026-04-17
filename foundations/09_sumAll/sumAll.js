2ea7a93f064fccdf2f3074644aebce56eb6bcb4c
Adetunji Okekunle <tjokekunle@gmail.com>
2026-04-17 19:50:58 +0100

    I made a function to add numbers in a range, checking they theyre all positive integers, i used the Number.isInteger() and a bunch of conditions

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let result = 0;
+   if (!(Number.isInteger(a))||(!Number.isInteger(b))|| a < 0 || b < 0){
+        return "ERROR";
+    } else if (a < b){
+        for (let i = a ; i < b+1 ; i++ ){
+            result+=i;
+        }
+    } else {
+        for (let i = b ; i<=a ; i++ ){
+            result+=i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
