574ae39584d2405806520e0e9959ffebe928c841
Adetunji Okekunle <tjokekunle@gmail.com>
2026-04-24 21:04:28 +0100

    This is a good use case to learn the string and array methods, there are a lot used here

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const palCheck = word.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return palCheck === palCheck.split("").reverse().join("");
 };
 
 // Do not edit below this line
